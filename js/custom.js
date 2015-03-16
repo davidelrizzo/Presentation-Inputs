04de8c6858058033caa588b266b241d623d87f99
Davide L Rizzo <davide@davidelrizzo.com>
2015-03-16 23:17:17 +1100

    Revisions & Optimisations
    Removed some pages
    Added better intro & conclusion

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var interval = window.setInterval(update, 500);
+function update() {
+  var val = $("#input-number").val();
+  if( !val ) val = "(empty string)"
+  $("#settings-output").text( val );
+  
+  $("#input-number").attr("min", $("#settings-min").val() );
+  $("#input-number").attr("max", $("#settings-max").val() );
+  $("#input-number").attr("step", $("#settings-step").val() );
+}
